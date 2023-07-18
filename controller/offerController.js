f892a838f51be764388a5d8b477eeb5e1242a912
Mathews Jacob <mathewsjacob198pb@gmail.com>
2023-07-18 18:04:00 +0530

    addiing productOffer

--- controller/offerController.js
+++ controller/offerController.js
@@ -8,24 +8,38 @@ const walletCollection=require('../model/walletModel')
 const offer={
     productOffer:async (req,res)=>{
         try {
-            const product= await productCollection.find({})
-            res.render('addProductOffer',{products:product})
+            const products = await productCollection.find({ offerprice: { $eq: 0 } });
+            const offered = await productCollection.find({ offerprice: { $ne: 0 } });
+            res.render('addProductOffer',{ products, offered,})
         } catch (error) {
             console.log(error.message);
         }
     },
-    addProductOffer:async(req,res)=>{
+    addProductOffer: async (req, res) => {
         try {
-            const productName = req.body.productname;
-const percentage = req.body.offerpercentage;
-const product = await productCollection.findOne({ productname: productName });
-const actualPrice = product.productprice;
-const offerPrice = actualPrice - (actualPrice * percentage / 100);
-res.redirect('/admin/productOffer')
+          const productName = req.body.productname;
+          const offerpercentage = req.body.offerpercentage; // Corrected line
+          const product = await productCollection.findOne({ productname: productName });
+          const actualPrice = product.productprice;
+          const offerprice = Math.floor( actualPrice - (actualPrice * offerpercentage / 100)); // Updated variable name
+            
+
+          const productUpdate = await productCollection.updateOne(
+            { productName }, // Updated variable name
+            { $set: { offerpercentage, offerprice} } // Updated variable name
+          );
+          res.redirect("/admin/productoffer");
         } catch (error) {
-            console.log(error.message);
+          console.log(error.message);
         }
-    }
+      },
+      
+      
+
+
+
+
+
 }
 
 module.exports=offer
